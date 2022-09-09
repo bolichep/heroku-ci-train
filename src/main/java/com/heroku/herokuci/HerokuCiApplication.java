9143a656168badf80f8b6a236afb02f02f1bf5a2
Pedro Boliche <bolichep@gmail.com>
2022-09-09 02:38:45 -0300

    Deploy setting

---
+++ src/main/java/com/heroku/herokuci/HerokuCiApplication.java
@@ -0,0 +1,13 @@
+package com.heroku.herokuci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuCiApplication.class, args);
+	}
+
+}
